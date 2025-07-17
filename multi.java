f88f30102cc2fa01d41db80919b6e9d3687d6848
VaibhavDev-sketch <vaibhavsagekar555@gmail.com>
2025-07-17 20:54:44 +0530

    multi

---
+++ multi.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class multi {
+    public static void main(String args[]) {
+        int a = 5;
+        int b = 10;
+        int product = a * b;
+        System.out.println("The product is: " + product);
+    }
+}
+
+
